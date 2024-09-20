18e4ba1043c62f09120cf957cca1b9e6dceb8b96
Pedro Henrique <pedrohenrique.goncalves753@gmail.com>
2024-09-20 16:58:45 -0300

    Header Quase finalizado e Inicio do Main

--- JavaScript/header.js
+++ JavaScript/header.js
@@ -1,37 +1,37 @@
-let categoria = document.getElementById('categoria');
-let modalCategoria = document.getElementById('modal_categoria');
+let categoria = document.getElementById("categoria");
+let modalCategoria = document.getElementById("modal_categoria");
 
 // Função para exibir o modal
 function modalVisibleCategoria() {
-    modalCategoria.style.display = 'block';
+  modalCategoria.style.display = "block";
 }
 
 // Função para ocultar o modal
 function modalInvisibleCategoria() {
-    modalCategoria.style.display = 'none';
+  modalCategoria.style.display = "none";
 }
 
 // Mostrar modal quando o mouse passar por cima de 'categoria'
-categoria.addEventListener('mouseenter', modalVisibleCategoria);
+categoria.addEventListener("mouseenter", modalVisibleCategoria);
 
 // Ocultar modal quando o mouse sair de 'categoria'
-categoria.addEventListener('mouseleave', modalInvisibleCategoria);
+categoria.addEventListener("mouseleave", modalInvisibleCategoria);
 
-let favorito = document.getElementById('favorito');
-let modalFavorito = document.getElementById('modal_favorito');
+let favorito = document.getElementById("favorito");
+let modalFavorito = document.getElementById("modal_favorito");
 
 // Função para exibir o modal
 function modalVisibleFavorito() {
-    modalFavorito.style.display = 'block';
+  modalFavorito.style.display = "block";
 }
 
 // Função para ocultar o modal
 function modalInvisibleFavorito() {
-    modalFavorito.style.display = 'none';
+  modalFavorito.style.display = "none";
 }
 
 // Mostrar modal quando o mouse passar por cima de 'favorito'
-favorito.addEventListener('mouseenter', modalVisibleFavorito);
+favorito.addEventListener("mouseenter", modalVisibleFavorito);
 
 // Ocultar modal quando o mouse sair de 'favorito'
-favorito.addEventListener('mouseleave', modalInvisibleFavorito);
+favorito.addEventListener("mouseleave", modalInvisibleFavorito);
